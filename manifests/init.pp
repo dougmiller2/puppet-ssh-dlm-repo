91dbda7f4e86562f04dc5ecfac0806b7a31a85d9
Doug Miller <to_doug@hotmail.com>
2018-06-05 01:16:24 +0000

    push to git before configuring Code Manager

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,24 @@
 #
 # Copyright 2018 Your name here, unless otherwise noted.
 #
-class ssh {
+class ssh(
+  String  $package_name,
+  String  $service_name,
+  String  $ensure,
+  String  $service_ensure,
+  Boolean $service_enable,
+  Boolean $permit_root_login = false,
+  Integer $port              = 22,
+) {
+  notify { "Test top scope var in a module":
+    message => "test top scope ${::test_top_scope}"
+  }
+  class { '::ssh::service': }
+  class { '::ssh::config': }
+  class { '::ssh::install': }
 
-
+  Class['::ssh::install']
+  -> Class['::ssh::config']
+  ~> Class['::ssh::service']
+  -> Class['ssh']
 }
